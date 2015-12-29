f274ecbd2780df3de789bf4eb63074a7b035839b
shadromero <producedbyshad@gmail.com>
2015-12-29 11:28:59 -0800

    Found css style problem and attributes correction

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 var colorChange = function () {
-	var currentClassName = document.getElementById('colorChange').className;
-
-	if (currentClassName == 'cool') {
-		document.getElementById('color').className = 'cool change';
-	} else {
-		document.getElementById('color').className = 'cool';
+	document.getElementById('color').className = 'cool color';
 	}
 
 var clickMe = function (error) {
