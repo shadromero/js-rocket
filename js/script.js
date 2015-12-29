17299c938f3a87587aa48abcc1d9678a8ff6e4dd
shadromero <producedbyshad@gmail.com>
2015-12-29 09:33:33 -0800

    New updates to js file and index page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var colorChange = function () {
-	document.getElementById('cool').className = 'cool change';
-}
+	var currentClassName = document.getElementById('colorChange').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('color').className = 'cool change';
+	} else {
+		document.getElementById('color').className = 'cool';
+	}
 
 var clickMe = function (error) {
 	alert(''+error);
